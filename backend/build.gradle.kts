bc5ee344505c8556f5e5469393a488bbf2a94ac4
rinta-toyoda <rinta.toyoda02@gmail.com>
2025-03-10 01:20:36 +1100

    1

---
+++ backend/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.jpa") version "1.9.25"
+}
+
+group = "com.moneyrecorder"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["testcontainersVersion"] = "1.16.2"
+
+dependencies {
+    // web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // db
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
+
+    // logging
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
+
+    // test
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("io.mockk:mockk:1.10.4")
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // test-containers
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
--- backend/src/main/kotlin/com/moneyrecorder/backend/BackEndApplication.kt
+++ backend/src/main/kotlin/com/moneyrecorder/backend/BackEndApplication.kt
@@ -1,4 +1,4 @@
-package com.moneyrecorder.back_end
+package com.moneyrecorder.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class BackEndApplication
 
 fun main(args: Array<String>) {
-	runApplication<BackEndApplication>(*args)
+    runApplication<BackEndApplication>(*args)
 }
